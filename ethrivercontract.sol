beb2e55209f4ff6aaa555cc3ffa3907f38b985a5
Theresa Whynot <132946455+theresa-whynot@users.noreply.github.com>
2023-09-15 10:57:45 -0400

    Add files via upload

--- ethrivercontract.sol
+++ ethrivercontract.sol
@@ -9,20 +9,22 @@ contract EthRiver {
 
     constructor() payable {
         owner = msg.sender;
-        require(msg.value >= 100, "Initial balance for the Faucet must be greater than or equal to 10,000 wei");
+        require(msg.value >= 10000000000000000, "Initial balance for the Faucet must be greater than or equal to 10,000,000,000,000,000 wei, or 10,000,000 gwei, or 0.01 ether.");
     }
 
 
-    function faucetDrip( address payable inputAddress) public payable {
-        dripInterval = 180;
-        drip = 10;
+    function faucetDrip( address payable inputAddress) public {
+        dripInterval = 43200;
+        drip = 1000000000;
 
         if(lastDrip[inputAddress]==0){
             lastDrip[inputAddress] = block.timestamp;
         } else {
-            require (block.timestamp >= lastDrip[inputAddress] + dripInterval, "Address can only receive drip once every 3 minutes");
+            require (block.timestamp >= lastDrip[inputAddress] + dripInterval, "Address can only receive drip once every 12 hours.");
         }
         
+        require(inputAddress == msg.sender, "You must be connected to the wallet address in your browser via MetaMask that you are requesting the drip for.");
+
         inputAddress.transfer(drip);
         lastDrip[inputAddress] = block.timestamp;
     }
@@ -32,10 +34,11 @@ contract EthRiver {
         totalDonations += msg.value;
     }
 
-    function getFaucetSummary() public view returns (uint, uint) {
+    function getFaucetSummary() public view returns (uint, uint, uint) {
         return (
             address(this).balance,
-            totalDonations
+            totalDonations,
+            drip
         );
     }
 }
